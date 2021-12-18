ca6215d132abedebeb6dc1a3c147f66a9d9e623e
merisimamovic <79166601+merisimamovic@users.noreply.github.com>
2021-12-18 18:52:17 +0100

    final

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -29,7 +29,7 @@
                             <input placeholder="0,00" class="input" type="text">
                         </div>
                 
-                        <a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1" class="btn-primary">Uporedi</a>
+                        <a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7" class="btn-primary">Uporedi</a>
 
                     </div>
                 
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,14 +4,14 @@
             <div class="nav__inner">
                 <img src="../assets/logo.svg" alt="" class="logo">
                 <ul class="nav__menu">
-                    <li><a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1">Uporedi transfer</a></li>
-                    <li><a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1">Opskrba voljenih</a></li>
-                    <li><a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1">Savjeti</a></li>
-                    <li><a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1">Pitanja</a></li>
-                    <li><a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1">O nama</a></li>
+                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Uporedi transfer</a></li>
+                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Opskrba voljenih</a></li>
+                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Savjeti</a></li>
+                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Pitanja</a></li>
+                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">O nama</a></li>
                 </ul>
             </div>
-            <a href="https://www.figma.com/file/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=0%3A1" class="btn-ghost">Registruj se</a>
+            <a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7" class="btn-ghost">Registruj se</a>
         </div>
         <div class="line"></div>
     </nav>
