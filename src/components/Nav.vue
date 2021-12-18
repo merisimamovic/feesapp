b3a0067f3e96d5c5a60d58fe023bac53971bbada
merisimamovic <79166601+merisimamovic@users.noreply.github.com>
2021-12-18 19:39:06 +0100

    finalfinalf inal

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,7 +5,7 @@
                 <img src="../assets/logo.svg" alt="" class="logo">
                 <ul class="nav__menu">
                     <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Uporedi transfer</a></li>
-                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Opskrba voljenih</a></li>
+                    <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Kupovina iz inostranstva</a></li>
                     <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Savjeti</a></li>
                     <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">Pitanja</a></li>
                     <li><a href="https://www.figma.com/proto/1Pv26rZNYSL0mh6TNul1wV/Feesless?node-id=1%3A7&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A7">O nama</a></li>
