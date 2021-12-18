a1ca6f8a3d9e75f66ce1539428564c1f78aa1103
merisimamovic <79166601+merisimamovic@users.noreply.github.com>
2021-12-18 01:11:40 +0100

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.svg">
+  <HelloWorld msg="Jebo ja mater svoju"/>
 </template>
 
 <script>
